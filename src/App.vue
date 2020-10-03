c5bcfb279ba78cb11b93adf1d0169d6cbfda9baf
Vitaliy <mr.prikota@gmail.com>
2020-10-03 22:41:14 +0300

    Added current time to header

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,58 @@
 <template>
-    <div id="app">
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-            <b-navbar-brand href="/">Вариант 2528</b-navbar-brand>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/">Вариант 2528</b-navbar-brand>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav class="ml-auto" type="dark">
-                    <div class="navbar-text">Прикота Виталий Александрович</div>
-                    <div class="navbar-text">P33113</div>
-                    <b-button type="primary" v-on:click="onLogOut">Log out</b-button>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto" type="dark">
+          <div class="navbar-text">Прикота Виталий Александрович</div>
+          <div class="navbar-text">P33113</div>
+          <div class="navbar-text">{{ this.time }}</div>
+          <b-button type="primary" v-on:click="onLogOut">Log out</b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 
 
-        <router-view></router-view>
+    <router-view></router-view>
 
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'app',
-    methods: {
-        onLogOut(evt) {
-            evt.preventDefault();
+  name: 'app',
+  methods: {
+    onLogOut(evt) {
+      evt.preventDefault();
 
-            sessionStorage.setItem("JSESSIONID", null);
-            this.$router.push("/");
-        }
+      sessionStorage.setItem("JSESSIONID", null);
+      this.$router.push("/");
+    },
+    updateTime() {
+      this.currentTime = new Date();
+      this.time = `${this.currentTime.getHours()}:${this.currentTime.getMinutes()}:${this.currentTime.getSeconds()}`;
     }
+  },
+  data() {
+    return {
+      currentTime: new Date(),
+      time: "00:00:00"
+    }
+  },
+  beforeMount() {
+    setInterval(function () {
+      this.updateTime();
+    }.bind(this), 100);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .navbar-text {
-    color: white;
-    padding: 1em;
+  color: white;
+  padding: 1em;
 }
 </style>
